52e0ed7479ef636ca2a128c59ff9c51b8a3956dc
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-07-16 01:03:52 +0200

    Added draw bounds to Player game object

--- app/src/main/java/com/visualstudioex3/canvasgame/game/entities/Player.kt
+++ app/src/main/java/com/visualstudioex3/canvasgame/game/entities/Player.kt
@@ -1,14 +1,16 @@
 package com.visualstudioex3.canvasgame.game.entities
 
+import android.graphics.Color
 import android.graphics.PointF
 import android.util.Log
 import com.visualstudioex3.canvasgame.R
 import com.visualstudioex3.canvasgame.engine.GameObject
 import com.visualstudioex3.canvasgame.engine.GameResources
 import com.visualstudioex3.canvasgame.engine.components.renderers.SpriteRenderer
+import com.visualstudioex3.canvasgame.engine.graphics.BitmapExtensions.Companion.getBounds
 import com.visualstudioex3.canvasgame.engine.graphics.GameRender
 
-class Player: GameObject() {
+class Player : GameObject() {
     init {
         renderer = SpriteRenderer(this)
         (renderer as SpriteRenderer).image =
@@ -23,5 +25,17 @@ class Player: GameObject() {
     }
 
     override fun onUpdate(deltaTime: Float) {
+    }
+
+    override fun onDraw() {
+        (renderer as SpriteRenderer).let {
+            if (it.image != null) {
+                GameRender.primitives.drawRect(
+                    it.image?.getBounds(transform.position)!!,
+                    transform.scale,
+                    color = Color.valueOf(1f, 0f, 0f, .25f),
+                )
+            }
+        }
     }
 }
