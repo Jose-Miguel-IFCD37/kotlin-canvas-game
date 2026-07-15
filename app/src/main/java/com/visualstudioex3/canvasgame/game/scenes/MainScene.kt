6e641f4a7dd3b96cd42b880b386d9cae2763e576
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-07-15 14:31:44 +0200

    Enabled fps counter in MainScene

--- app/src/main/java/com/visualstudioex3/canvasgame/game/scenes/MainScene.kt
+++ app/src/main/java/com/visualstudioex3/canvasgame/game/scenes/MainScene.kt
@@ -1,6 +1,7 @@
 package com.visualstudioex3.canvasgame.game.scenes
 
 import com.visualstudioex3.canvasgame.engine.Scene
+import com.visualstudioex3.canvasgame.engine.graphics.GameRender
 import com.visualstudioex3.canvasgame.game.entities.HelloWorld
 import com.visualstudioex3.canvasgame.game.entities.Player
 
@@ -8,6 +9,8 @@ class MainScene: Scene() {
     override fun onCreate() {
         gameObjects.add(HelloWorld())
         gameObjects.add(Player())
+
+        GameRender.showFPSCounter = true
     }
 
     override fun onDestroy() {
