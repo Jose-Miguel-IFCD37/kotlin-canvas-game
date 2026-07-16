70cd8cef5b9d4f3c09f097e586e43b2d61f87441
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-07-17 01:30:04 +0200

    Implemented onTap input event on Player game object

--- app/src/main/java/com/visualstudioex3/canvasgame/game/entities/Player.kt
+++ app/src/main/java/com/visualstudioex3/canvasgame/game/entities/Player.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import com.visualstudioex3.canvasgame.R
 import com.visualstudioex3.canvasgame.engine.GameObject
 import com.visualstudioex3.canvasgame.engine.GameResources
+import com.visualstudioex3.canvasgame.engine.components.input.InputTouch
 import com.visualstudioex3.canvasgame.engine.components.physics.SpriteCollider
 import com.visualstudioex3.canvasgame.engine.components.renderers.SpriteColliderRenderer
 import com.visualstudioex3.canvasgame.engine.components.renderers.SpriteRenderer
@@ -12,6 +13,10 @@ import com.visualstudioex3.canvasgame.engine.graphics.RenderManager
 
 class Player : GameObject() {
     init {
+        addComponent<InputTouch>().onTap = { offset ->
+            transform.translate(offset.x)
+            Log.d("Player::onTap", "$offset")
+        }
         addComponent<SpriteCollider>()
         addComponent<SpriteColliderRenderer>()
         addComponent<SpriteRenderer>()
