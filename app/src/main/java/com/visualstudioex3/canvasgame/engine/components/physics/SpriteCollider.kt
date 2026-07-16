0296a727bbc8f58aa2f372e5908d9bb1217c293d
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-07-16 23:33:51 +0200

    Implemented SpriteCollider component
    To define the collider bounds based on sprite bounds.

---
+++ app/src/main/java/com/visualstudioex3/canvasgame/engine/components/physics/SpriteCollider.kt
@@ -0,0 +1,25 @@
+package com.visualstudioex3.canvasgame.engine.components.physics
+
+import android.graphics.RectF
+import com.visualstudioex3.canvasgame.engine.GameObject
+import com.visualstudioex3.canvasgame.engine.components.IComponent
+import com.visualstudioex3.canvasgame.engine.components.renderers.SpriteRenderer
+import com.visualstudioex3.canvasgame.engine.graphics.BitmapExtensions.Companion.getBounds
+
+class SpriteCollider(
+    override val gameObject: GameObject
+) : IComponent {
+    private var spriteRenderer: SpriteRenderer? = null
+
+    override var enable: Boolean = true
+    var bounds = RectF()
+        private set
+
+    override fun update(deltaTime: Float) {
+        if (spriteRenderer == null)
+            spriteRenderer = gameObject.getComponent<SpriteRenderer>()
+
+        bounds = spriteRenderer?.image
+            ?.getBounds(gameObject.transform.position) ?: RectF()
+    }
+}
