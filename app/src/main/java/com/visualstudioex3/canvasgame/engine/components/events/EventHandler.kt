5d68a0e67ab0d6c3a2365acc95b60143bb10391d
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-07-17 14:52:59 +0200

    Implemented EventHandler component
    Component to ease implement event handlers for continuous event
    listeners, on enter listener and on exit listener.

---
+++ app/src/main/java/com/visualstudioex3/canvasgame/engine/components/events/EventHandler.kt
@@ -0,0 +1,29 @@
+package com.visualstudioex3.canvasgame.engine.components.events
+
+import com.visualstudioex3.canvasgame.engine.GameObject
+import com.visualstudioex3.canvasgame.engine.components.IComponent
+
+class EventHandler(
+    override val gameObject: GameObject
+) : IComponent {
+    private var eventRaised: Boolean = false
+    override var enable: Boolean = true
+
+    var condition: () -> Boolean = { true }
+    var onEvent: () -> Unit = {}
+    var onEnter: () -> Unit = {}
+    var onExit: () -> Unit = {}
+
+    override fun update(deltaTime: Float) {
+        if (condition()) {
+            onEvent()
+            if (!eventRaised) {
+                onEnter()
+                eventRaised = true
+            } else {
+                onExit()
+                eventRaised = false
+            }
+        }
+    }
+}
