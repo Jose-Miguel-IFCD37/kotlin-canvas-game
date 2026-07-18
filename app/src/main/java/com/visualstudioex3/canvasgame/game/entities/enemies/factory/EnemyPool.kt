fef7329678f0e8467eedd9256d35b8ddcb630d67
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-07-18 23:40:02 +0200

    Fixed enemyTypes list initialization on EnemyPool
    Because the ObjectPool abstract constructor is executed before the
    initialization of the derived class (EnemyPool in this case) we moved
    the list in a companion object, to made it static var, and with this,
    forced to initialize before instances.

--- app/src/main/java/com/visualstudioex3/canvasgame/game/entities/enemies/factory/EnemyPool.kt
+++ app/src/main/java/com/visualstudioex3/canvasgame/game/entities/enemies/factory/EnemyPool.kt
@@ -1,20 +1,25 @@
 package com.visualstudioex3.canvasgame.game.entities.enemies.factory
 
 import com.visualstudioex3.canvasgame.engine.ObjectPool
+import com.visualstudioex3.canvasgame.engine.scenes.SceneManager
 import com.visualstudioex3.canvasgame.game.entities.enemies.Asteroid
-import com.visualstudioex3.canvasgame.game.entities.enemies.EnemyShip
 import com.visualstudioex3.canvasgame.game.entities.enemies.BaseEnemy
+import com.visualstudioex3.canvasgame.game.entities.enemies.EnemyShip
 
 class EnemyPool(
     instances: Int
 ): ObjectPool<BaseEnemy>(instances) {
-    private val enemyTypes = listOf(
-        Asteroid::class.java,
-        EnemyShip::class.java
-    )
+    companion object {
+        private val enemyTypes = listOf(
+            Asteroid::class.java,
+            EnemyShip::class.java
+        )
+    }
 
     override fun onCreateInstance(): BaseEnemy =
-        enemyTypes.random()
+        (enemyTypes.random()
             .getDeclaredConstructor()
-            .newInstance() as BaseEnemy
+            .newInstance() as BaseEnemy).apply {
+            SceneManager.scene.gameObjects.add(this)
+        }
 }
