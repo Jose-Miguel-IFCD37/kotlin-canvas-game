9bc345dd7fd46cf5dac287b0b35940faa5d350a2
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-07-18 23:29:57 +0200

    Removed debug log message from InputManager cacheState method

--- app/src/main/java/com/visualstudioex3/canvasgame/engine/input/InputManager.kt
+++ app/src/main/java/com/visualstudioex3/canvasgame/engine/input/InputManager.kt
@@ -1,7 +1,6 @@
 package com.visualstudioex3.canvasgame.engine.input
 
 import android.graphics.PointF
-import android.util.Log
 import com.visualstudioex3.canvasgame.engine.graphics.RenderManager
 
 class InputManager {
@@ -14,7 +13,6 @@ class InputManager {
 
     fun cacheState(gesture: TouchGestures, offset: PointF) {
         cacheState = InputState(gesture, RenderManager.camera.toCameraCoordinates(offset))
-        Log.d("InputManager::cacheState", "Cached ${gesture.name} $offset")
     }
 
     fun update() {
