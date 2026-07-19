5d1b71218e9c3f06292a9152a4e888f69009b428
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-07-19 22:23:07 +0200

    Removed commend explanations from RenderManager class

--- app/src/main/java/com/visualstudioex3/canvasgame/engine/graphics/RenderManager.kt
+++ app/src/main/java/com/visualstudioex3/canvasgame/engine/graphics/RenderManager.kt
@@ -11,19 +11,6 @@ import com.visualstudioex3.canvasgame.engine.graphics.drawcommands.TextAlign
 import com.visualstudioex3.canvasgame.engine.graphics.drawcommands.TextDrawCommand
 import com.visualstudioex3.canvasgame.engine.graphics.drawprocessors.DrawProcessorFactory
 
-/*
-    En videojuegos y multimedia en general el renderizado se implementa mediante una arquitectura de
-    comandos. Los comandos simplemente definen los parametros de una operacion de dibujo:
-     - Coordenadas de posicion
-     - Rotacion
-     - Escalado
-     - Material (Color, textura, fuente de texto, geometria 3D...)
-
-     Cada objeto del juego compone y envia estos comandos a una lista del renderizador. Este, cuando
-     se le llama a presentar en pantalla el resultado, ejecuta secuencialmente toda la lista de
-     comandos usando procesadores. Cada procesador se encarga de una operacion grafica concreta:
-     dibujar texto, dibujar una imagen, dibujar un modelo 3D...
- */
 class RenderManager(
     private val surfaceHolder: SurfaceHolder
 ) {
@@ -31,11 +18,6 @@ class RenderManager(
         private lateinit var _camera: Camera
         private val commands = ArrayDeque<IDrawCommand>()
 
-        /*
-            Generalmente los motores suelen permitir definir multiples camaras para realizar
-            composiciones complejas o efectos visuales avanzados. En este motor simplemente usaremos
-            una unica camara.
-         */
         val camera: Camera
             get() = _camera
 
