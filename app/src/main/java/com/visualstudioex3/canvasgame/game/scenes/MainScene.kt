ecf5206c5735d813f6295b9f15a96e01a09515a0
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-07-19 02:59:46 +0200

    Added EnemySpawner to MainScene

--- app/src/main/java/com/visualstudioex3/canvasgame/game/scenes/MainScene.kt
+++ app/src/main/java/com/visualstudioex3/canvasgame/game/scenes/MainScene.kt
@@ -4,11 +4,13 @@ import com.visualstudioex3.canvasgame.engine.scenes.Scene
 import com.visualstudioex3.canvasgame.engine.graphics.RenderManager
 import com.visualstudioex3.canvasgame.game.entities.HelloWorld
 import com.visualstudioex3.canvasgame.game.entities.Player
+import com.visualstudioex3.canvasgame.game.entities.enemies.factory.EnemySpawner
 
 class MainScene: Scene() {
     override fun onCreate() {
         gameObjects.add(HelloWorld())
         gameObjects.add(Player())
+        gameObjects.add(EnemySpawner())
 
         RenderManager.showFPSCounter = true
     }
