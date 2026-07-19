2404e8f1f8da6003e92304f618d3e907cba407a9
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-07-19 02:48:53 +0200

    Refactorized SpriteCollider and removed EventHandler component

--- app/src/main/java/com/visualstudioex3/canvasgame/engine/components/physics/SpriteCollider.kt
+++ app/src/main/java/com/visualstudioex3/canvasgame/engine/components/physics/SpriteCollider.kt
@@ -2,35 +2,22 @@ package com.visualstudioex3.canvasgame.engine.components.physics
 
 import android.graphics.RectF
 import com.visualstudioex3.canvasgame.engine.GameObject
-import com.visualstudioex3.canvasgame.engine.components.events.EventHandler
+import com.visualstudioex3.canvasgame.engine.components.IComponent
 import com.visualstudioex3.canvasgame.engine.components.renderers.SpriteRenderer
 import com.visualstudioex3.canvasgame.engine.graphics.extensions.BitmapExtensions.Companion.getBounds
-import com.visualstudioex3.canvasgame.engine.graphics.RenderManager
 
 class SpriteCollider(
     override val gameObject: GameObject
-) : EventHandler(gameObject) {
-    private var spriteRenderer = gameObject.getComponent<SpriteRenderer>()
+) : IComponent {
+    private var spriteRenderer: SpriteRenderer? = null
 
     override var enable: Boolean = true
     var bounds = RectF()
         private set
 
-    var onCollision: (GameObject) -> Unit = {}
-    var onCollisionEnter: (GameObject) -> Unit = {}
-    var onCollisionExit: (GameObject) -> Unit = {}
-    var onExitCameraBounds: () -> Unit = {}
-    var onEnterCameraBounds: () -> Unit = {}
-
-    init {
-        condition = {
-            RenderManager.camera.bounds.intersect(bounds)
-        }
-        onEnter = onEnterCameraBounds
-        onExit = onExitCameraBounds
-    }
-
     override fun update(deltaTime: Float) {
+        if (spriteRenderer == null)
+            spriteRenderer = gameObject.getComponent<SpriteRenderer>()
         bounds = spriteRenderer?.image
             ?.getBounds(gameObject.transform.position) ?: RectF()
     }
