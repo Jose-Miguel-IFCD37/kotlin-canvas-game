ce641ca3535de019062394f765789358920e12f6
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-07-20 14:59:53 +0200

    Minor refactor

--- app/src/main/java/com/visualstudioex3/canvasgame/game/entities/player/components/PlayerBulletSpawner.kt
+++ app/src/main/java/com/visualstudioex3/canvasgame/game/entities/player/components/PlayerBulletSpawner.kt
@@ -3,7 +3,7 @@ package com.visualstudioex3.canvasgame.game.entities.player.components
 import com.visualstudioex3.canvasgame.engine.GameObject
 import com.visualstudioex3.canvasgame.engine.IComponent
 import com.visualstudioex3.canvasgame.engine.Timer
-import com.visualstudioex3.canvasgame.game.entities.player.PlayerBulletPool
+import com.visualstudioex3.canvasgame.game.entities.player.services.PlayerBulletPool
 import com.visualstudioex3.canvasgame.game.services.settings.FactorySettingsData
 import com.visualstudioex3.canvasgame.game.services.settings.GameSettings
 
@@ -17,7 +17,7 @@ class PlayerBulletSpawner(
         interval = settings.spawnTime
         onTime = {
             instances.getInstance()!!.apply {
-                transform?.position = gameObject.transform.position
+                transform.position = gameObject.transform.position
             }
         }
     }
