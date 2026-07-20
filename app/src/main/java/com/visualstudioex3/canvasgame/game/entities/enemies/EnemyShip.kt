0f9f15652730a7407a5a3b2e0fd49159609d43e3
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-07-20 13:28:08 +0200

    Added EnemyBulletSpawner component to EnemyShip game object

--- app/src/main/java/com/visualstudioex3/canvasgame/game/entities/enemies/EnemyShip.kt
+++ app/src/main/java/com/visualstudioex3/canvasgame/game/entities/enemies/EnemyShip.kt
@@ -1,5 +1,6 @@
 package com.visualstudioex3.canvasgame.game.entities.enemies
 
+import com.visualstudioex3.canvasgame.game.entities.enemies.components.EnemyBulletSpawner
 import com.visualstudioex3.canvasgame.game.services.settings.EnemyShipSettingsData
 import com.visualstudioex3.canvasgame.game.services.settings.GameSettings
 
@@ -8,6 +9,7 @@ class EnemyShip: BaseEnemy() {
         .settings.enemySettings.enemyShipSettings
 
     init {
+        addComponent<EnemyBulletSpawner>()
         settings.bitmapResourceIds.forEach {
             addSprite(it)
         }
