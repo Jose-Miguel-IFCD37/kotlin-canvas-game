3697693ae575cff452b81df5b4c5d87dea410ab4
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-07-22 01:29:01 +0200

    Implemented Unity's Mathf.Approximately method to safe compare 2 float values

--- app/src/main/java/com/visualstudioex3/canvasgame/engine/MathF.kt
+++ app/src/main/java/com/visualstudioex3/canvasgame/engine/MathF.kt
@@ -2,6 +2,8 @@ package com.visualstudioex3.canvasgame.engine
 
 class MathF {
     companion object {
+        const val EPSILON: Float = 1.1754944E-38f
+
         fun max(a: Float, b: Float): Float =
             kotlin.math.max(a, b)
 
@@ -13,5 +15,21 @@ class MathF {
 
         fun lerp(start: Float, stop: Float, fraction: Float): Float =
             androidx.compose.ui.util.lerp(start, stop, fraction)
+
+        fun abs(value: Float): Float =
+            kotlin.math.abs(value)
+
+        // Basado en la implementacion de Unity Mathf.Approximately:
+        // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/Mathf.Approximately.html
+        // https://github.com/Unity-Technologies/UnityCsReference/blob/master/Runtime/Export/Math/Mathf.cs#L263
+        fun approximately(a: Float, b: Float): Boolean =
+            this.abs(a - b) <
+                    this.max(
+                        0.000001f * this.max(
+                            this.abs(a),
+                            this.abs(b)
+                        ),
+                        EPSILON
+                    )
     }
 }
